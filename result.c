aba79cee3a658fa449022630f79e3d31e144b3e4
wldbjs0219 <55428633+wldbjs0219@users.noreply.github.com>
2019-11-21 07:01:55 +0900

    12

--- result.c
+++ result.c
@@ -16,17 +16,72 @@
 
 
 // calculate the card sum and see if : 1. under 21, 2. over 21, 3. blackjack
-int calcStepResult(int playernum) {  
-
-
+int calcStepResult(int user) {
+	
+	for(i=0;i<cardcnt[user];i++)
+	{
+		cardhold[user][i]=getCardNum(cardhold[user][i]);
+		cardSum[user]+=cardhold[user][i];
+	}
+	
+	return cardSum[user];
 }
 
-
-int checkResult(int playernum) {                  // playernum: you-0, player-1~, dealer- n_user
+int checkResult(int user) { 
+	
+	printf("------- ROUND %d RESULT -------",roundindex);
+	
+	//your result
+	//case: win
+	if(cardSum[0]<=21)
+	{
+		if(cardSum[0]==21)
+		{
+			printf("--> your result: win! due to Blackjack --> dollar(%d)\n",dollar[0]);
+		}
+		
+		else if(cardSum[0]<21 && cardSum[0]>=cardSum[n_user])
+		{
+			printf(" --> your result: win! sum(%d) --> dollar(%d)\n",cardSum[0],dollar[0]);
+		}
+	//lose
+		else if (cardSum[0]<21 && cardSum[0]<cardSum[n_user])
+		{
+			printf(" --> your result: lose! sum(%d) --> dollar(%d)\n",cardSum[0],dollar[0]);
+		}
+		else
+		{
+			printf("--> your result: lose! due to overflow --> dollar(%d)\n",dollar[0]);
+		}
+		
+	//players result
+	for(i=1;i<n_user;i++)
+	{
+	//case: win
+		if(cardSum[i]<=21)
+		{
+			if(cardSumi0]==21)
+			{
+				printf("--> %dth Player result: win! due to Blackjack --> dollar(%d)\n",i,dollar[i]);
+			}
+		
+			else if(cardSum[0]<21 && cardSum[0]>=cardSum[n_user])
+			{
+				printf(" --> %dth Player result: win! sum(%d) --> dollar(%d)\n",i,cardSum[i],dollar[i]);
+			}
+	//lose
+			else if (cardSum[0]<21 && cardSum[0]<cardSum[n_user])
+			{
+				printf(" --> %dth Player result: lose! sum(%d) --> dollar(%d)\n",i,cardSum[i],dollar[i]);
+			}	
+			else
+			{
+				printf("--> %dth Player result: lose! due to overflow --> dollar(%d)\n",i,dollar[i]);
+			}
+	               
 }
 
 int checkWinner() {
-	
-	printf("     ->your result: ")
+
 	
 }
